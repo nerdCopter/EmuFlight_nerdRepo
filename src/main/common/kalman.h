a088a482767c935cf0dbf56afb8f3277c26572a3
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2020-04-19 10:11:17 -0500

    Marinus Kalman covariance without cross-axis influence

--- src/main/common/kalman.h
+++ src/main/common/kalman.h
@@ -12,9 +12,6 @@ typedef struct variance
     float xVar;
     float yVar;
     float zVar;
-    float xyCoVar;
-    float xzCoVar;
-    float yzCoVar;
 
     uint32_t windex;
     float xWindow[MAX_KALMAN_WINDOW_SIZE];
