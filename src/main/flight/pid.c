2d5f2baf8b51c66c1e427a16d09fc14b67918c08
Quick-Flash <46289813+Quick-Flash@users.noreply.github.com>
2020-04-26 10:30:54 -0500

    cherrypick update to smart smoothing #197

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -141,8 +141,8 @@ void resetPidProfile(pidProfile_t *pidProfile)
 
         //WitchCraft, Smart_DTerm_Smoothing Defaults
         .dFilter = {
-            [PID_ROLL] = { 2, 5 },     // wC, smartSmoothing
-            [PID_PITCH] = { 2, 5 },    // wC, smartSmoothing
+            [PID_ROLL] = { 2, 0 },     // wC, smartSmoothing
+            [PID_PITCH] = { 2, 0 },    // wC, smartSmoothing
             [PID_YAW] = { 0, 0 },       // wC, smartSmoothing
         },
 
@@ -1108,7 +1108,7 @@ void pidController(const pidProfile_t *pidProfile, const rollAndPitchTrims_t *an
 
               float dDeltaMultiplier;
               if (smart_dterm_smoothing[axis] > 0) {
-                  dDeltaMultiplier = constrainf(fabsf(dDelta + previousdDelta[axis]) / (2 * smart_dterm_smoothing[axis]), 0.5f, 1.0f);
+                  dDeltaMultiplier = constrainf(fabsf((dDelta + previousdDelta[axis]) / (4 * smart_dterm_smoothing[axis])) + 0.5, 0.5f, 1.0f); //smooth transition from 0.5-1.0f for the multiplier.
                   dDelta = dDelta * dDeltaMultiplier;
                   previousdDelta[axis] = dDelta;
                   DEBUG_SET(DEBUG_SMART_SMOOTHING, axis, dDeltaMultiplier * 1000.0f);
