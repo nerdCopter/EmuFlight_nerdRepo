208c1fe6c85619a0355708d7fd1fd2d54b749978
Andrey Semjonov <50518855+AndreySemjonov@users.noreply.github.com>
2020-07-02 09:07:34 -0500

    fixing iTerm more better faster than King Kraken

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -74,8 +74,20 @@ typedef struct pidf_s {
     uint8_t I;
     uint8_t D;
     uint16_t F; // Only angle mode still uses FF
-
 } pidf_t;
+
+typedef enum {
+    ITERM_RELAX_OFF,
+    ITERM_RELAX_RP,
+    ITERM_RELAX_RPY,
+    ITERM_RELAX_RP_INC,
+    ITERM_RELAX_RPY_INC
+} itermRelax_e;
+
+typedef enum {
+    ITERM_RELAX_GYRO,
+    ITERM_RELAX_SETPOINT
+} itermRelaxType_e;
 
 typedef struct dFilter_s {
     uint8_t Wc;
@@ -125,6 +137,9 @@ typedef struct pidProfile_s {
     uint8_t throttle_boost;                 // how much should throttle be boosted during transient changes 0-100, 100 adds 10x hpf filtered throttle
     uint8_t throttle_boost_cutoff;          // Which cutoff frequency to use for throttle boost. higher cutoffs keep the boost on for shorter. Specified in hz.
     uint8_t iterm_rotation;                 // rotates iterm to translate world errors to local coordinate system
+    uint8_t iterm_relax_type;               // Specifies type of relax algorithm
+    uint8_t iterm_relax_cutoff;             // This cutoff frequency specifies a low pass filter which predicts average response of the quad to setpoint
+    uint8_t iterm_relax;                    // Enable iterm suppression during stick input
     uint8_t motor_output_limit;             // Upper limit of the motor output (percent)
     int8_t auto_profile_cell_count;         // Cell count for this profile to be used with if auto PID profile switching is used
 } pidProfile_t;
