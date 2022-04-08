25a0005de5c29a0a73274693c83e707b80266afb
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2022-04-08 13:56:41 -0500

    CRAZYBEEF4RXV31
    nerdCopter's feeble attempt at adding v3.1 ICM20689

---
+++ src/main/target/CRAZYBEEF4RXV31/target.mk
@@ -0,0 +1,19 @@
+F411_TARGETS    += $(TARGET)
+
+FEATURES        += VCP SDCARD
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_icm20689.c \
+            drivers/max7456.c \
+            drivers/rx/rx_cc2500.c \
+						rx/cc2500_common.c \
+            rx/cc2500_frsky_shared.c \
+            rx/cc2500_frsky_d.c \
+						rx/cc2500_frsky_x.c \
+						rx/cc2500_redpine.c \
+            rx/cc2500_sfhss.c
+ifeq ($(TARGET), CRAZYBEEF4FSV31)
+TARGET_SRC += \
+            drivers/rx/rx_a7105.c \
+            rx/flysky.c
+endif
