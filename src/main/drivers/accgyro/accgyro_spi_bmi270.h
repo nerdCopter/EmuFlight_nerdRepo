757fec3c7778384d5aad94e1f0aaee0c43cc0d66
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-06-20 13:55:19 -0500

    BMI270 - initial port Dusking;  WIP; same resource shift problem as other branch

--- src/main/drivers/accgyro/accgyro_spi_bmi270.h
+++ src/main/drivers/accgyro/accgyro_spi_bmi270.h
@@ -1,19 +1,21 @@
 /*
- * This file is part of Cleanflight and Betaflight.
+ * This file is part of EmuFlight. It was ported from 
+ * Betaflight/Betaflight and FlightNG/ATBetaflight.
  *
- * Cleanflight and Betaflight are free software. You can redistribute
- * this software and/or modify this software under the terms of the
- * GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
  *
- * Cleanflight and Betaflight are distributed in the hope that they
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this software.
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,8 +23,69 @@
 #pragma once
 
 #include "drivers/bus.h"
+#include "drivers/exti.h"
 
+// BMI270 registers (not the complete list)
+typedef enum {
+    BMI270_REG_CHIP_ID = 0x00,
+    BMI270_REG_ERR_REG = 0x02,
+    BMI270_REG_STATUS = 0x03,
+    BMI270_REG_ACC_DATA_X_LSB = 0x0C,
+    BMI270_REG_GYR_DATA_X_LSB = 0x12,
+    BMI270_REG_SENSORTIME_0 = 0x18,
+    BMI270_REG_SENSORTIME_1 = 0x19,
+    BMI270_REG_SENSORTIME_2 = 0x1A,
+    BMI270_REG_EVENT = 0x1B,
+    BMI270_REG_INT_STATUS_0 = 0x1C,
+    BMI270_REG_INT_STATUS_1 = 0x1D,
+    BMI270_REG_INTERNAL_STATUS = 0x21,
+    BMI270_REG_TEMPERATURE_LSB = 0x22,
+    BMI270_REG_TEMPERATURE_MSB = 0x23,
+    BMI270_REG_FIFO_LENGTH_LSB = 0x24,
+    BMI270_REG_FIFO_LENGTH_MSB = 0x25,
+    BMI270_REG_FIFO_DATA = 0x26,
+    BMI270_REG_FEAT_PAGE = 0x2F,
+    BMI270_REG_FEATURES_0_GYR_GAIN_STATUS = 0x38,
+    BMI270_REG_FEATURES_0_GYR_CAS = 0x3C,
+    BMI270_REG_FEATURES_1_G_TRIG_1 = 0x32,
+    BMI270_REG_FEATURES_1_GEN_SET_1 = 0x34,
+    BMI270_REG_ACC_CONF = 0x40,
+    BMI270_REG_ACC_RANGE = 0x41,
+    BMI270_REG_GYRO_CONF = 0x42,
+    BMI270_REG_GYRO_RANGE = 0x43,
+    BMI270_REG_AUX_CONF = 0x44,
+    BMI270_REG_FIFO_DOWNS = 0x45,
+    BMI270_REG_FIFO_WTM_0 = 0x46,
+    BMI270_REG_FIFO_WTM_1 = 0x47,
+    BMI270_REG_FIFO_CONFIG_0 = 0x48,
+    BMI270_REG_FIFO_CONFIG_1 = 0x49,
+    BMI270_REG_SATURATION = 0x4A,
+    BMI270_REG_INT1_IO_CTRL = 0x53,
+    BMI270_REG_INT2_IO_CTRL = 0x54,
+    BMI270_REG_INT_LATCH = 0x55,
+    BMI270_REG_INT1_MAP_FEAT = 0x56,
+    BMI270_REG_INT2_MAP_FEAT = 0x57,
+    BMI270_REG_INT_MAP_DATA = 0x58,
+    BMI270_REG_INIT_CTRL = 0x59,
+    BMI270_REG_INIT_DATA = 0x5E,
+    BMI270_REG_GYR_CRT_CONF = 0x69,
+    BMI270_REG_IF_CONF = 0x6B,
+    BMI270_REG_ACC_SELF_TEST = 0x6D,
+    BMI270_REG_GYR_SELF_TEST_AXES = 0x6E,
+    BMI270_REG_OFFSET_6 = 0x77,
+    BMI270_REG_PWR_CONF = 0x7C,
+    BMI270_REG_PWR_CTRL = 0x7D,
+    BMI270_REG_CMD = 0x7E,
+} bmi270Register_e;
+
+extern int8_t bmi270CasFactor;
+
+// Contained in accgyro_spi_bmi270_init.c which is size-optimized
 uint8_t bmi270Detect(const busDevice_t *bus);
 bool bmi270SpiAccDetect(accDev_t *acc);
 bool bmi270SpiGyroDetect(gyroDev_t *gyro);
+
+// Contained in accgyro_spi_bmi270.c which is speed-optimized
 uint8_t bmi270InterruptStatus(gyroDev_t *gyro);
+bool bmi270AccRead(accDev_t *acc);
+bool bmi270GyroRead(gyroDev_t *gyro);
--- src/main/target/SKYSTARSF7HDPRO/target.mk
+++ src/main/target/SKYSTARSF7HDPRO/target.mk
@@ -2,9 +2,9 @@ F7X2RE_TARGETS  += $(TARGET)
 FEATURES        += VCP ONBOARDFLASH
 
 TARGET_SRC = \
-						drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
-						drivers/accgyro/accgyro_spi_bmi270.c \
-						drivers/accgyro/accgyro_spi_mpu6000.c \
+            drivers/accgyro/accgyro_spi_bmi270_init.c \
+            drivers/accgyro/accgyro_spi_bmi270.c \
+            drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_mpu.c \
 						drivers/accgyro/accgyro_mpu6500.c \
 						drivers/accgyro/accgyro_spi_mpu6500.c \
