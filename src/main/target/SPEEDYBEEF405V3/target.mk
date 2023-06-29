1eaf358f510c5aee4ac06e3f5b46aa5391dd034b
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-06-29 10:53:55 -0500

    BMI270 - speedybeef405v3 normal driver

--- src/main/target/SPEEDYBEEF405V3/target.mk
+++ src/main/target/SPEEDYBEEF405V3/target.mk
@@ -2,7 +2,7 @@ F405_TARGETS    += $(TARGET)
 FEATURES        += VCP SDCARD
 
 TARGET_SRC = \
-					drivers/accgyro/accgyro_bmi270_maximum_fifo.c \
+					drivers/accgyro/accgyro_bmi270.c \
 					drivers/accgyro/accgyro_spi_bmi270.c \
 					drivers/accgyro/accgyro_mpu.c \
 					drivers/accgyro/accgyro_spi_mpu6000.c \
